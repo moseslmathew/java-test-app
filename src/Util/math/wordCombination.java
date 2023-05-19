6c77f7c002204fcc5b6877f53d8f1e75d349a597
moseslmathew <moseslmathew@gmail.com>
2023-05-19 12:51:31 +0530

    Commit-p

---
+++ src/Util/math/wordCombination.java
@@ -0,0 +1,18 @@
+package Util.math;
+
+public class wordCombination {
+    private String givenWords="Test";
+
+    public String getGivenWords() {
+        return givenWords;
+    }
+
+    public void setGivenWords(String givenWords) {
+        this.givenWords = givenWords;
+    }
+
+    public void calc(){
+
+
+    }
+}
