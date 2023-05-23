503423a8e7ddf57df023bd0cb1ef68d1b910f5d5
Mathew <Moses.Mathew@finastra.com>
2023-05-23 08:25:48 +0530

    Math Update

--- src/Util/math/wordCombination.java
+++ src/Util/math/wordCombination.java
@@ -3,27 +3,28 @@ package Util.math;
 import java.util.*;
 
 public class wordCombination {
-    private String givenWord = "Testt";
+    private String givenWord = "Malayalam";
 
     public String getGivenWord() {
         return givenWord;
     }
 
     public void calc() {
-        char[] givenWordArray = givenWord.toLowerCase().toCharArray();
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        for (char ch : givenWordArray) {
+        char[] chArray = givenWord.toLowerCase().toCharArray();
+        char[] chArrayUnique=removeDuplicate(chArray);
+        int totalCharCount =calculus.getFactorial(chArray.length);
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : chArrayUnique) {
             map.put(ch,getRepeatCount(givenWord,ch));
         }
-        System.out.println(givenWordArray.length);
-        System.out.println(map);
-        System.out.println(calculus.getFactorial(4));
-        int temp=0;
-        for(int i=0;i<map.size();i++){
-            temp=calculus.getFactorial(givenWordArray)/calculus.getFactorial()
+
+
+        for(Map.Entry<Character, Integer> item : map.entrySet()){
+            totalCharCount=totalCharCount/calculus.getFactorial(item.getValue());
         }
 
-
+        System.out.println("Total number Combinations that can be formed with word \""+ givenWord+ "\" is: "+totalCharCount);
     }
 
     public int getRepeatCount(String str, char c) {
@@ -34,6 +35,25 @@ public class wordCombination {
                 count++;
         }
         return count;
+    }
+
+    public char[] removeDuplicate(char[] c){
+        char[] ch=c;
+
+        int temp=0;
+        HashSet<Character> charSet=new HashSet<>();
+
+        for(char item:ch){
+            charSet.add(item);
+        }
+        char[] ch2=new char[charSet.size()];
+        for(Character itr:charSet)
+        {
+            ch2[temp]=itr.charValue();
+            temp++;
+        }
+
+       return ch2;
     }
 
 }
