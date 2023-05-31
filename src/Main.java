41c1243059fab9a55e33af65d58646057560754b
moseslmathew <moseslmathew@gmail.com>
2023-05-31 09:29:02 +0530

    Commit-p

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ import Util.math.wordCombination;
 import basics.TestRecursion;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        wordCombination obj=new wordCombination();
-        wordCombination.printWordCombinations("1234","123",1);
+        ArrayList<String> combinationList=new ArrayList<>();
+        wordCombination.printWordCombinations("1234","",combinationList);
+        System.out.println("Result:"+combinationList);
     }
 
 
--- src/Util/math/wordCombination.java
+++ src/Util/math/wordCombination.java
@@ -1,33 +1,31 @@
 package Util.math;
 
+import com.sun.security.jgss.GSSUtil;
+
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class wordCombination {
-    private String givenWord = "Malayalam";
 
-    public String getGivenWord() {
-        return givenWord;
-    }
-
-    public void calc() {
-        char[] chArray = givenWord.toLowerCase().toCharArray();
-        char[] chArrayUnique=removeDuplicate(chArray);
-        int totalCharCount =calculus.getFactorial(chArray.length);
+    public static void combinationCount(String str) {
+        char[] chArray = str.toLowerCase().toCharArray();
+        char[] chArrayUnique = removeDuplicate(chArray);
+        int totalCharCount = calculus.getFactorial(chArray.length);
 
         HashMap<Character, Integer> map = new HashMap<>();
         for (char ch : chArrayUnique) {
-            map.put(ch,getRepeatCount(givenWord,ch));
+            map.put(ch, getRepeatCount(str, ch));
         }
 
 
-        for(Map.Entry<Character, Integer> item : map.entrySet()){
-            totalCharCount=totalCharCount/calculus.getFactorial(item.getValue());
+        for (Map.Entry<Character, Integer> item : map.entrySet()) {
+            totalCharCount = totalCharCount / calculus.getFactorial(item.getValue());
         }
 
-        System.out.println("Total number Combinations that can be formed with word \""+ givenWord+ "\" is: "+totalCharCount);
+        System.out.println("Total number Combinations that can be formed with word \"" + str + "\" is: " + totalCharCount);
     }
 
-    public int getRepeatCount(String str, char c) {
+    public static int getRepeatCount(String str, char c) {
         int count = 0;
         char[] charArray = str.toLowerCase().toCharArray();
         for (char ch : charArray) {
@@ -37,38 +35,43 @@ public class wordCombination {
         return count;
     }
 
-    public char[] removeDuplicate(char[] c){
-        char[] ch=c;
+    public static char[] removeDuplicate(char[] c) {
 
-        int temp=0;
-        HashSet<Character> charSet=new HashSet<>();
+        int temp = 0;
+        HashSet<Character> charSet = new HashSet<>();
 
-        for(char item:ch){
+        for (char item : c) {
             charSet.add(item);
         }
-        char[] ch2=new char[charSet.size()];
-        for(Character itr:charSet)
-        {
-            ch2[temp]=itr.charValue();
+        char[] ch2 = new char[charSet.size()];
+        for (Character itr : charSet) {
+            ch2[temp] = itr;
             temp++;
         }
 
-       return ch2;
+        return ch2;
     }
 
-    public static void printWordCombinations(String str, String perm, int index){
-        for(int i=0;i<str.length();i++)
-        {
-            char currentChar=str.charAt(i);
-            System.out.print(currentChar);
-            String newStr=str.substring(0,i)+str.substring((i+1));
-            System.out.println(newStr);
+    //tree structure explained in RecursiveTreeCombination.bmp
+    public static void printWordCombinations(String str,String currentPermResult,ArrayList<String> resultList) {
+
+        if(str.length()==0) {
+            System.out.println("Result of the tree: " + currentPermResult);
+            resultList.add(currentPermResult);
+        }
+
+        for (int i = 0; i <str.length(); i++) {
+            char placedChar = str.charAt(i);
+            String remainingChars = str.substring(0, i) + str.substring((i + 1));
+            System.out.printf("placedChar:%c remainingChars:%s currentPermResult:%s",placedChar,remainingChars,currentPermResult+placedChar);
+            System.out.println();
+            printWordCombinations(remainingChars, currentPermResult+placedChar,resultList);
         }
 
     }
 
-    public static String removeCharAtIndex(String str,int index){
-        StringBuilder strB=new StringBuilder(str);
+    public static String removeCharAtIndex(String str, int index) {
+        StringBuilder strB = new StringBuilder(str);
         strB.deleteCharAt(index);
         return strB.toString();
     }
